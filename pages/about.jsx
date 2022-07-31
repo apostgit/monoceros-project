a24a0c7e8c6b1e59d4ffe31371ebd226c3d00a86
ApostGit <54368456+apostgit@users.noreply.github.com>
2022-07-31 18:44:53 +0300

    update

--- pages/about.jsx
+++ pages/about.jsx
@@ -4,40 +4,49 @@ const about = () => {
   return (
     <div className={styles.about}>
       <div className={styles.components}>
+        <div className={styles.item}>
+          <div className={styles.inner}>
+            <p>Monoceros is one of the 88 official galaxy constellations of the known universe.</p>
+            <p>The Monoceros Project is about the future of art in a digital world.</p>
+            <p>Monoceros aspires to bring physical art up to speed with a world that is changing at an interstellar pace.</p>
+            <p>We want physical art to continue to exist and adapt in an era that seems to be primarily digital.</p>
+            <p>We wish to empower the creators of art - artists, first and foremost - as well as the ecosystem of art as we understand it today, which includes galleries, exhibitions, museums, Schools of Fine Arts, to name the most obvious.</p>
+            <p>The Monoceros Project seeks to establish long lasting relationships with art ecosystems and empower them in this transitional period.</p>
+          </div>
+        </div>
+
         <div className={styles.item} id='bridge'>
           <div className={styles.inner}>
             <p className={styles.header}>NFTs - The bridge between Physical and Digital</p>
-            <p>In our view, in order to achieve that we need to bring physical art up to speed with digital art.</p>
-            <p>The unexpected and parabolic growth of Non-Fungible Tokens or NFTs is a strong sign that it is very easy to create a “digital twin” for a “real” thing.</p>
-            <p>By turning a physical piece of art into an NFT, one creates a unique “digital twin” of the real physical asset.</p>
-            <p>This opens up many possibilities for art - to sell a digital copy instead of a physical copy, much faster and cheaper; to create NFTs of art with high value and other restrictions e.g., safety and storage restrictions, and in this way make it more accessible and easier to monetize it. This monetization could then be used to enhance art in the real world.</p>
-            <p>Examples of our vision of empowering art could be:</p>
+            <p>The unexpected and parabolic growth of Non-Fungible Tokens (NFTs) is a strong sign of how easy it is to create a digital version of a 'real' thing.</p>
+            <p>By turning physical art into an NFT, one creates a unique digital version of it.</p>
+            <p>This opens many new 'skies' for art - to create a digital copy instead of a physical copy much faster and cheaper; to easily create art NFTs and make it accessible and easier to monetize. This monetization could then be used to enhance art in the real world.</p>
+            <p>Our vision of empowering art encompasses, among others:</p>
             <ul>
-              <li>Scholarships to exceptional Art students</li>
               <li>Funding to Schools of Fine Arts</li>
-              <li>Sponsoring of exhibitions</li>
+              <li>Scholarships to exceptional Art students</li>
+              <li>Sponsoring of exhibitions and art spaces</li>
             </ul>
-            <p>and many many other ideas, for which you can reach us at monoceros@...</p>
           </div>
         </div>
 
         <div className={styles.item} id='marketplace'>
           <div className={styles.inner}>
-            <p className={styles.header}>Monoceros Marketplace</p>
-            <p>Our goals are meant to be achieved through our proprietary NFT marketplace.</p>
-            <p>In our marketplace we feature NFTs that represent physical pieces of art, of known origin and authenticity.</p>
-            <p>Artists, galleries, collectors and museums are all free to upload their NFT collection in our marketplace and sell the “digital twin” of a real piece of art. Of course, one could try to acquire the physical piece as well, but this is beyond the scope of our platform.</p>
+            <p className={styles.header}>Monoceros NFT Platform</p>
+            <p>Our goals are meant to be achieved through our proprietary NFT platform.</p>
+            <p>In our platform we feature NFTs that represent physical pieces of art of known origin and quality.</p>
+            <p>Artists, galleries, collectors, and museums are free to upload their NFT collection in our platform and promote the digital versions of their physical pieces of art. Of course, one could acquire the physical piece as well, but this is beyond the scope of our platform.</p>
           </div>
         </div>
 
         <div className={styles.item} id='wediffer'>
           <div className={styles.inner}>
             <p className={styles.header}>Why we are Different</p>
-            <p>Our marketplace is different for one simple reason - it features “real” art of quality.</p>
-            <p>This statement requires further qualification. We believe in the absolute freedom of art and the expression of artists in any way they see fit, away from “labeling”.</p>
-            <p>However, there is in broad terms, an objective consensus of what is art, and also, quality art.</p>
-            <p>Another reason which is even more disturbing and obvious in the NFT space, is the commercialization of and packaging of everything as art; or put otherwise, there is a vast flow of information and NFTs, and it is difficult to the untrained eye to distinguish between quality art and random art.</p>
-            <p>In conclusion, in our marketplace we feature what is objectively regarded as real, quality art with a curated selection of NFTs.</p>
+            <p>Our platform is different for one simple reason - it features 'quality art'.</p>
+            <p>This statement requires further qualification; we believe in the absolute freedom of art and the expression of artists in any way they see fit, away from 'labeling'.</p>
+            <p>However, there is an 'objective' consensus of what is art - and especially, quality art.</p>
+            <p>A disturbing issue in the NFT space is the packaging and commercialization of everything as 'art'. This vast flow of information makes difficult the distinction between quality art and random 'art' to the untrained eye.</p>
+            <p>Our platform features a curated selection of NFTs that originates from physical pieces of 'objectively' quality art - and awaits you to explore it.</p>
           </div>
         </div>
       </div>
--- pages/index.jsx
+++ pages/index.jsx
@@ -7,12 +7,7 @@ export default function Home() {
         <div className={styles.bg}>
           <div className={styles.blur}>
             <div className={styles.info}>
-              <p>Monoceros is one of the 88 official galaxy constellations of the known universe.</p>
-              <p>The Monoceros Project is about the future of art in a digital world.</p>
-              <p>Monoceros aspires to bring physical art up to speed with a world that is changing at an interstellar pace.</p>
-              <p>We want physical art to continue to exist and adapt in an era that seems to be primarily digital.</p>
-              <p>We wish to empower the creators of art - artists, first and foremost - as well as the ecosystem of art as we understand it today, which includes galleries, exhibitions, museums, Schools of Fine Arts, to name the most obvious.</p>
-              <p>The Monoceros Project seeks to establish long lasting relationships with art ecosystems and empower them in this transitional period.</p>
+              <p>Physical Art in a Digital World</p>
 
               <button onClick={() => window.open('/about', '_self')}>
                 The Project
